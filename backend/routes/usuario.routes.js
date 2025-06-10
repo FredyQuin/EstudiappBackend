055a68770b6ff6c4c2d15ac5c32c317d7237f8bd
FredyQuin <freres29YT@gmail.com>
2025-06-10 18:20:18 -0500

    cambios a usuario, ya se puede hacer CRUD

--- backend/routes/usuario.routes.js
+++ backend/routes/usuario.routes.js
@@ -2,15 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usuarioCtrl = require('../controllers/usuario.controller');
 
-
-// Todas las rutas requieren autenticación de admin
-
-
-router.get('/', (req, res) => {
-  // Ahora puedes acceder a req.user.id (del middleware)
-  usuarioCtrl.getAll(req, res);
-});
-
+// Rutas públicas sin autenticación
+router.get('/', usuarioCtrl.getAll);
 router.post('/', usuarioCtrl.create);
 router.put('/:id', usuarioCtrl.update);
 router.delete('/:id', usuarioCtrl.delete);
